46f24a8d295191f4bd0a5a4f849c3ace6e44eda2
Patrick595 <patrickbuong@gmail.com>
2020-03-15 02:49:43 +0300

    defined a function that at login the new projects posted by users should be displayed

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import *
+from .forms import ProjectUpload,ProfileForm
+from django.contrib.auth.decorators import login _required
+from rest_framework import viewsets
+from .serializers import PostSerializer,ProfileSerializer
+
+def home(request):
+    projects = Post.objects.all()
+    return render(request, 'myprojects/index.html',{"projects":projects})
+
+@login_required (login_url='/accounts/login/?next=/')
+    
