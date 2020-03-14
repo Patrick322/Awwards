489f3a7d8350c6e35e147370998987a238163658
Patrick595 <patrickbuong@gmail.com>
2020-03-15 01:42:18 +0300

    added class profile in the models where users will enter the information of them selves

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime as dt
+from django .contrib.auth.models import User
+from tinymce.modes import HTMLField
+from django.db.models.signals import post save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    
