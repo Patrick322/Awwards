20bc4a586b6fd1480b5474c0a9357f4fdfdb2c25
Patrick595 <patrickbuong@gmail.com>
2020-03-15 02:09:12 +0300

    created class posts where users will post the ratings

--- awwards/models.py
+++ awwards/models.py
@@ -38,4 +38,11 @@ class Profile(models.Model):
 
 @receiver(post_save,sender=User)
 def create_profile(sender, instance,created,**kwargs):
-    if           
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save,sender=User)
+def save _profile(sender, instance,**kwargs):
+    instace.profile.save()
+
+class Post(models.Model):                       
